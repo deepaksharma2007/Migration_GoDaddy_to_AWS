aee742ed5e5b71d38b627589fdceb40a564c3878
Deepak Sharma <ds9199027@gmail.com>
2025-03-02 15:46:06 +0530

    Adding variables to get more flexibility & increase re-usuability of code

---
+++ variables.tf
@@ -0,0 +1,27 @@
+
+variable "aws_region" {}
+
+variable "vpc_name" {}
+
+variable "vpc_cidr" {}
+
+variable "public_subnet_name" {}
+
+variable "public_sub_availability_zone" {}
+
+variable "private_sub_availability_zone" {}
+
+variable "private_subnet_name" {}
+
+variable "public_subnet_cidr" {}
+
+variable "private_subnet_cidr" {}
+
+variable "instance_ami" {}
+
+variable "instance_type" {}
+
+variable "key_name" {}
+
+variable "ec2_instance_name" {}
+
