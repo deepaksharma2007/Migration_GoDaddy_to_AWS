e88eb600da57ef319209822cd600be550ef546b5
Deepak Sharma <ds9199027@gmail.com>
2025-04-15 17:53:18 +0530

    added rds variables

--- variables.tf
+++ variables.tf
@@ -7,15 +7,13 @@ variable "vpc_cidr" {}
 
 variable "public_subnet_name" {}
 
-variable "public_sub_availability_zone" {}
-
-variable "private_sub_availability_zone" {}
-
 variable "private_subnet_name" {}
 
 variable "public_subnet_cidr" {}
 
-variable "private_subnet_cidr" {}
+variable "private_subnet_cidr_1" {}
+
+variable "private_subnet_cidr_2" {}
 
 variable "instance_ami" {}
 
@@ -24,4 +22,13 @@ variable "instance_type" {}
 variable "key_name" {}
 
 variable "ec2_instance_name" {}
+
+variable "db_username" {}
+
+variable "db_password" {}
+
+variable "mysql_version" {}
+
+variable "mysql_instance_type" {}
+
 
