817f0da06d7d200c0fcfabf35e72db9d1573429a
Deepak Sharma <ds9199027@gmail.com>
2025-04-15 17:52:18 +0530

    Output file updated to get RDS url

--- output.tf
+++ output.tf
@@ -3,3 +3,8 @@ output "public_instance_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.public_instance.public_ip
 }
+
+output "rds_endpoint" {
+  description = "Endpoint of the RDS instance"
+  value       = aws_db_instance.rds_instance.endpoint
+}
